e6c5171c7257bb2681eb69f8bb8aa91b533f64a2
Ray <guokangf@gmail.com>
2022-03-28 23:46:41 +0800

    leaning

---
+++ by-example/20/20-5-1_pipes.rs
@@ -0,0 +1,34 @@
+use std::error::Error;
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
+
+static PANGRAM: &'static str =
+"the quick brown fox jumped over the lazy dog\n";
+
+fn main() {
+    // 启动`wc`命令
+    let process = match Command::new("wc")
+                                .stdin(Stdio::piped())
+                                .stdout(Stdio::piped())
+                                .spawn() {
+        Err(why) => panic!("couldn't spawn wc: {}", why.description()),
+        Ok(process) => process,
+    };
+
+    // 将字符串写入`wc`的`stdin`
+    // `stdin`拥有`Option<ChildStdin>`类型，不过我们已经知道这个实例不为空值，因而可以直接`unwrap`它
+    match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
+        Err(why) => panic!("couldn't write to wc stdin: {}", why.description()),
+        Ok(_) => println!("sent pangram to wc"),
+    }
+
+    // 因为`stdin`在上面调用后就不再存活，所以它被`drop`了，管道也被关闭
+    // 这点非常重要，否则`wc`就不会开始处理我们刚刚发送的输入
+
+    // `stdout`字段也拥有`Option<ChildStdout>`类型，所以必须解包
+    let mut s = String::new();
+    match process.stdout.unwrap().read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read wc stdout: {}", why.description()),
+        Ok(_) => print!("wc responded with:\n{}", s),
+    }
+}
