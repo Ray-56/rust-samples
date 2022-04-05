847453eba2c895e402e9e884d5a9fa8a2b94e085
Ray <guokangf@gmail.com>
2022-04-05 15:39:44 +0800

    blog_os 1.1

---
+++ my_blog_os/READMD.md
@@ -0,0 +1,3 @@
+# Rust blog os
+
+from https://www.bilibili.com/medialist/play/ml966774072/BV1gC4y1p7zm?oid=795746169&otype=2
--- my_blog_os/src/main.rs
+++ my_blog_os/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // 不链接 Rust 标准库
+#![no_main] // 禁用所有的 Rust 层级的入口点
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // 不重整函数名
+pub extern "C" fn _start() -> ! {
+    // 因为编译器会寻找一个名为`_start`的函数，所以这个函数就是入口点
+    loop {}
+}
+
+/// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
