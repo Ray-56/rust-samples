7ed3cf6c51cf4cdd140a806ba24eea7725ec53f2
Ray <guokangf@gmail.com>
2022-05-14 13:39:50 +0800

    feat

--- my_blog_os/READMD.md
+++ my_blog_os/READMD.md
@@ -1,3 +1,8 @@
 # Rust blog os
 
 from https://www.bilibili.com/medialist/play/ml966774072/BV1gC4y1p7zm?oid=795746169&otype=2
+
+
+## 一些坑
+
+https://zhuanlan.zhihu.com/p/475947094
--- my_blog_os/src/main.rs
+++ my_blog_os/src/main.rs
@@ -1,11 +1,25 @@
 #![no_std] // 不链接 Rust 标准库
 #![no_main] // 禁用所有的 Rust 层级的入口点
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
     // 因为编译器会寻找一个名为`_start`的函数，所以这个函数就是入口点
+    
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
