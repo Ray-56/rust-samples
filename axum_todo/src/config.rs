8b314b8e661a526d3197a5ff12b9159efdd67981
ray <ray.fan@airudder.com>
2022-05-17 00:55:36 +0800

    fix: [Axum todo] get data from .env

--- axum_todo/src/config.rs
+++ axum_todo/src/config.rs
@@ -3,13 +3,13 @@
 use serde::Deserialize;
 
 /// Web 配置
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct WebConfig {
     pub addr: String,
 }
 
 /// 应用配置
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Config {
     pub web: WebConfig,
 }
@@ -17,10 +17,13 @@ pub struct Config {
 impl Config {
     /// 从环境变量中初始化配置
     pub fn from_env() -> Result<Self, config::ConfigError> {
-        let mut cfg = config::Config::builder().set_default()?;
-        cfg.build()?;
-        // cfg.merge();
-        // cfg.merge(config::Environment::default())?;
-        // todo!()
+        // let mut cfg = config::Config::new();
+        // cfg.merge(config::Environment::new())?;
+        // cfg.try_into()
+        let config = config::Config::builder()
+            .add_source(config::Environment::default())
+            .build()?;
+
+        config.try_deserialize()
     }
 }
--- axum_todo/src/main.rs
+++ axum_todo/src/main.rs
@@ -1,11 +1,42 @@
-// mod config;
-
-use config::Environment;
 use dotenv::dotenv;
+
+use serde::Deserialize;
+
+mod config;
+
+/// Web 配置
+#[derive(Deserialize, Debug)]
+pub struct WebConfig {
+    pub addr: String,
+}
+
+/// 应用配置
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub web: WebConfig,
+}
 
 #[tokio::main]
 async fn main() {
-    let a = dotenv().ok();
-    // let env = Environment;
-    println!("{:#?}", a);
+    // 解析 .env 文件
+    dotenv().ok();
+
+    // for (key, value) in env::vars() {
+    //     println!("{}: {}", key, value);
+    // }
+
+    // let config = config::Config::builder()
+    //     .add_source(config::Environment::default())
+    //     .build()
+    //     .unwrap();
+
+    // let app: Config = config.try_deserialize().unwrap();
+
+    // let config = config::Config::parse();
+
+    let cfg = config::Config::from_env().expect("初始化配置失败");
+    println!("{:?}", cfg);
+
+    // let cfg = config::Config::from_env().expect("初始化配置失败");
+    // println!("{:?}", cfg); // Config { web: WebConfig { addr: "0.0.0.0:9527" } }
 }
