70e8374c23931b31af5089dca1a355d54aa4ce3c
ray <ray.fan@airudder.com>
2022-05-18 15:12:45 +0800

    Axum todo

--- axum_todo/src/config.rs
+++ axum_todo/src/config.rs
@@ -17,9 +17,6 @@ pub struct Config {
 impl Config {
     /// 从环境变量中初始化配置
     pub fn from_env() -> Result<Self, config::ConfigError> {
-        // let mut cfg = config::Config::new();
-        // cfg.merge(config::Environment::new())?;
-        // cfg.try_into()
         let config = config::Config::builder()
             .add_source(config::Environment::default())
             .build()?;
--- axum_todo/src/main.rs
+++ axum_todo/src/main.rs
@@ -1,29 +1,161 @@
-use axum::{routing::get, Router};
-use dotenv::dotenv;
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock}, time::Duration, net::SocketAddr,
+};
+use axum::{
+    extract::{Path, Query},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, patch},
+    Extension, Json, Router, error_handling::HandleErrorLayer, BoxError,
+};
 
-mod config;
-mod handler;
-mod error;
+use serde::{Deserialize, Serialize};
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use uuid::Uuid;
 
-/// 定义自己的 Result
-type Result<T> = std::result::Result<T, error::AppError>;
+// use dotenv::dotenv;
+// mod config;
 
 #[tokio::main]
 async fn main() {
     // 解析 .env 文件
-    dotenv().ok();
+    // dotenv().ok();
+    // let cfg = config::Config::from_env().expect("初始化配置失败");
+    // println!("{:?}", cfg);
 
-    let app = Router::new().route("/", get(handler::usage));
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "example_todos=debug,tower_http=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    let cfg = config::Config::from_env().expect("初始化配置失败");
-    println!("{:?}", cfg);
+    let db = Db::default();
 
-    // 绑定到配置文件设置的地址
-    axum::Server::bind(&cfg.web.addr.parse().unwrap())
+    // Compose the routes
+    let app = Router::new()
+        .route("/todos", get(todos_index).post(todos_create))
+        .route("/todos/:id", patch(todos_update).delete(todos_delete))
+        // Add middleware to all routes
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                    if error.is::<tower::timeout::error::Elapsed>() {
+                        Ok(StatusCode::REQUEST_TIMEOUT)
+                    } else {
+                        Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {}", error),
+                        ))
+                    }
+                }))
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http())
+                .layer(Extension(db))
+                .into_inner(),
+        );
+    
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
-        .unwrap();
+        .unwrap()
+}
 
-    // let cfg = config::Config::from_env().expect("初始化配置失败");
-    // println!("{:?}", cfg); // Config { web: WebConfig { addr: "0.0.0.0:9527" } }
+// the query parameters for todos index
+#[derive(Debug, Deserialize, Default)]
+pub struct Pagination {
+    pub offset: Option<usize>,
+    pub limit: Option<usize>,
+}
+
+async fn todos_index(
+    pagination: Option<Query<Pagination>>,
+    Extension(db): Extension<Db>,
+) -> impl IntoResponse {
+    let todos = db.read().unwrap();
+
+    let Query(pagination) = pagination.unwrap_or_default();
+
+    let todos = todos
+        .values()
+        .skip(pagination.offset.unwrap_or(0))
+        .take(pagination.limit.unwrap_or(usize::MAX))
+        .cloned()
+        .collect::<Vec<_>>();
+
+    Json(todos)
+}
+
+#[derive(Debug, Deserialize)]
+struct CreateTodo {
+    text: String,
+}
+
+async fn todos_create(
+    Json(input): Json<CreateTodo>,
+    Extension(db): Extension<Db>,
+) -> impl IntoResponse {
+    let todo = Todo {
+        id: Uuid::new_v4(),
+        text: input.text,
+        completed: false,
+    };
+
+    db.write().unwrap().insert(todo.id, todo.clone());
+
+    (StatusCode::CREATED, Json(todo))
+}
+
+#[derive(Debug, Deserialize)]
+struct UpdateTodo {
+    text: Option<String>,
+    completed: Option<bool>,
+}
+
+async fn todos_update(
+    Path(id): Path<Uuid>,
+    Json(input): Json<UpdateTodo>,
+    Extension(db): Extension<Db>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let mut todo = db
+        .read()
+        .unwrap()
+        .get(&id)
+        .cloned()
+        .ok_or(StatusCode::NOT_FOUND)?;
+
+    if let Some(text) = input.text {
+        todo.text = text;
+    }
+
+    if let Some(completed) = input.completed {
+        todo.completed = completed;
+    }
+
+    db.write().unwrap().insert(todo.id, todo.clone());
+
+    Ok(Json(todo))
+}
+
+async fn todos_delete(Path(id): Path<Uuid>, Extension(db): Extension<Db>) -> impl IntoResponse {
+    if db.write().unwrap().remove(&id).is_some() {
+        StatusCode::NO_CONTENT
+    } else {
+        StatusCode::NOT_FOUND
+    }
+}
+
+type Db = Arc<RwLock<HashMap<Uuid, Todo>>>;
+
+#[derive(Debug, Serialize, Clone)]
+struct Todo {
+    id: Uuid,
+    text: String,
+    completed: bool,
 }
